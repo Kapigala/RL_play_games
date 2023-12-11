1ebe1258ec7bd23cbed90de0f1d7a419bf0062a3
Kapigala <kacper.galek@gmail.com>
2023-12-11 02:56:37 +0100

    rearange

--- WHG/open_pyautogui.py
+++ WHG/open_pyautogui.py
@@ -7,20 +7,23 @@ import sys
 import pyscreenshot
 from datetime import date
 import os
-from PIL import Image, ImageOps,ImageGrab
+from PIL import Image, ImageOps,ImageGrab,ImageChops
+import cv2
 
 #game_path=r"C:\Users\Kacper\PycharmProjects\RL_play_games\WHG\game_source\the-worlds-hardest-game.exe"
 game_path=r"C:\Users\Kacper\PycharmProjects\RL_play_games\WHG\game_source\WHGTrainer.exe"
-#for i in range(N): #instances work
 
 p = subprocess.Popen(game_path,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,)
-#time.sleep(1.2)
-time.sleep(1.5) #old pc setup
+#time.sleep(1.5) #old pc setup
+time.sleep(1.2)
 
 s_location = py.locateOnScreen('game_finder.png')
 x,y = py.center(s_location)
 
 luc, rdc = (x-274,y-166),(x+272,y+230)
+
+#py.click(luc)
+#time.sleep(3)
 
 py.click((x,y))
 
@@ -69,19 +72,96 @@ try:
 except:
     pass
 
+try:
+    os.mkdir(fr"../WHG/Train_sessions/{session}/lvl_1")
+except:
+    pass
+
+lvl=1
 # INIT controlers
 print("TRAINING SESSION START")
-alive = pyscreenshot.grab(bbox=(rdc[0]-50, luc[1], rdc[0], luc[1]+21)) #if change was death
-#ifwin = pyscreenshot.grab(bbox=(rdc[0] - 298, luc[1], rdc[0]-250, luc[1] + 21))  # if change was death
-ifwin = ImageGrab.grab().load()[luc[0],luc[1]+50]
+
 delta_reward=0
 try:
     coins_now=len(list(py.locateAllOnScreen('coin_color.png',confidence=0.8)))
 except:
     coins_now = 0
 
-
 while p.poll() is None: #Start loop
+    move_time=time.time()
+    pic = ImageOps.grayscale(pyscreenshot.grab(bbox=(luc[0], luc[1]+25, rdc[0], rdc[1]-25)))
+
+    #coins_now
+    print("CURRENT STATE:",coins_now,delta_reward,t)
+
+    #if train
+    pic.save(fr"../WHG/Train_sessions/{session}/lvl_{lvl}/{live}_{t}.png")
+
+    move = random.choice([
+                            ["left"],["up"],["right"],["down"],
+                            [],["down","left"],["down","left"],["up","right"],["up","left"]
+                            ])
+    print("Action",move)
+
+    py.FAILSAFE = False
+    for m in move:
+        py.keyDown(m)
+    for m in move:
+        py.keyUp(m)
+    py.FAILSAFE = True
+
+    t += 1
+
+    if pic.getpixel((1,30)) == 220:
+        delta_reward +=  1000
+        print(f"attempt {live} wins - score:{delta_reward}")
+        # zapisz jako wynik sesji t
+        #ifwin = full_sc.crop((245,0,305,21))
+        #time.sleep(0.1) #could be longer
+
+        #DURING PRETREIN
+        #p.kill()
+        lvl += 1
+        try:
+            os.mkdir(fr"../WHG/Train_sessions/{session}/lvl_{lvl}")
+        except:
+            pass
+
+        try:
+            coins_now=len(list(py.locateAllOnScreen('coin_color.png',confidence=0.8)))
+        except:
+            coins_now = 0
+
+        break
+        time.sleep(1) #wait to go next
+        coins_now = len(list(py.locateAllOnScreen('coin_color.png', confidence=0.8, region=(luc[0], luc[1], rdc[0], rdc[1]))))
+
+        """
+        #next lvl init TODO:
+        -current coint
+        -new full_sc
+        """
+
+    try:
+        list(py.locateOnScreen('yellow_square.png', confidence=0.9, region=(luc[0], luc[1], rdc[0], rdc[1])))[0]
+        delta_reward -= 1
+        if coins_now != 0:
+            bon = coins_now
+            coins_now = len(list(py.locateAllOnScreen('coin_color.png', confidence=0.8,region=(luc[0], luc[1], rdc[0], rdc[1]))))
+            delta_reward += 10*(bon-coins_now)
+    except:
+        delta_reward -= 100
+        print(f"attempt {live} fails - score:{delta_reward}")
+        # zapisz jako wynik sesji t
+        time.sleep(0.1)
+        try:
+            coins_now = len(
+                list(py.locateAllOnScreen('coin_color.png', confidence=0.8
+                                          , region=(luc[0], luc[1], rdc[0], rdc[1]))))
+        except:
+            coins_now = 0
+        live += 1
+        t=0
 
     #terminations
     if t==20:
@@ -95,64 +175,9 @@ while p.poll() is None: #Start loop
             coins_now = len(list(py.locateAllOnScreen('coin_color.png', confidence=0.8, region=(luc[0], luc[1], rdc[0], rdc[1]))))
         except:
             coins_now = 0
-        alive = pyscreenshot.grab(bbox=(rdc[0] - 50, luc[1], rdc[0], luc[1] + 21))
         delta_reward = 0
-        #time.sleep(0.1)
         live += 1
         t = 0
-
-    if alive != pyscreenshot.grab(bbox=(rdc[0]-50, luc[1], rdc[0], luc[1]+21)):
-        delta_reward -= 100
-        print(f"attempt {live} fails - score:{delta_reward}")
-        # zapisz jako wynik sesji t
-        alive = pyscreenshot.grab(bbox=(rdc[0] - 50, luc[1], rdc[0], luc[1] + 21))
-        try:
-            coins_now = len(
-                list(py.locateAllOnScreen('coin_color.png', confidence=0.8, region=(luc[0], luc[1], rdc[0], rdc[1]))))
-        except:
-            coins_now = 0
-        #time.sleep(0.1)
-        live += 1
-        t=0
-
-
-    if ifwin != ImageGrab.grab().load()[luc[0],luc[1]+50]:
-        delta_reward +=  1000
-        print(f"attempt {live} wins - score:{delta_reward}")
-        # zapisz jako wynik sesji t
-        ifwin = pyscreenshot.grab(bbox=(rdc[0] - 298, luc[1], rdc[0]-250, luc[1] + 21))
-        #time.sleep(0.1) #could be longer
-        time.sleep(2) #wait to go next
-        coins_now = len(list(py.locateAllOnScreen('coin_color.png', confidence=0.8, region=(luc[0], luc[1], rdc[0], rdc[1]))))
-
-        #DURING PRETREIN
-        p.kill()
-
-    if coins_now != 0:
-        bon=coins_now
-        coins_now = len(list(py.locateAllOnScreen('coin_color.png', confidence=0.8,region=(luc[0], luc[1], rdc[0], rdc[1]))))
-        delta_reward += 10*(bon-coins_now)
-
-    #state
-    pic = ImageOps.grayscale(pyscreenshot.grab(bbox=(luc[0], luc[1], rdc[0], rdc[1])))
-    #coins_now
-    print("CURRENT STATE:",pic,coins_now,delta_reward,t)
-
-    #if train
-    pic.save(fr"../WHG/Train_sessions/{session}/{live}_{t}.png")
-
-    move = random.choice([
-                            ["left"],["up"],["right"],["down"],
-                            [],["down","left"],["down","left"],["up","right"],["up","left"]
-                            ])
-    print("Action",move)
-
-    for m in move:
-        py.keyDown(m)
-    for m in move:
-        py.keyUp(m)
-
-    delta_reward -= 1
-    t += 1
+    print("loop_time:",time.time()- move_time)
 p.kill()
 print("QUIT")
