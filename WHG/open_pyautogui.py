cc768795753a288e29bcf3ac3e7a8b9cc33df19b
Kapigala <kacper.galek@gmail.com>
2023-12-21 01:33:48 +0100

    ready_logic_track

--- WHG/open_pyautogui.py
+++ WHG/open_pyautogui.py
@@ -6,7 +6,7 @@ import random
 import sys
 from datetime import date
 import os
-from PIL import Image, ImageOps,ImageGrab,ImageChops
+from PIL import Image, ImageOps
 import mss
 import keyboard
 from pyscreeze import locateAll,locate
@@ -15,7 +15,7 @@ from pyscreeze import locateAll,locate
 game_path=r"C:\Users\Kacper\PycharmProjects\RL_play_games\WHG\game_source\WHGTrainer.exe"
 
 p = subprocess.Popen(game_path,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,)
-sleep(1.5) #old pc setup
+#sleep(1.5) #old pc setup
 sleep(1.3)
 
 s_location = py.locateOnScreen('game_finder.png')
@@ -95,7 +95,7 @@ coin = ImageOps.grayscale(Image.open('coin_color.png'))
 
 if not os.path.isfile(fr"../WHG/Train_sessions/{session}/raport_{lvl}/reward_track.txt"):
     f = open(fr"../WHG/Train_sessions/{session}/raport_{lvl}/reward_track.txt", "w")
-    f.write("live,time_step,action,reward\n")
+    f.write("live,time_step,action,reward,sigle_reward\n")
 else:
     f = open(fr"../WHG/Train_sessions/{session}/raport_{lvl}/reward_track.txt", "a")
 
@@ -119,6 +119,7 @@ while p.poll() is None: #Start loop
                             ])
     print("Action",move)
 
+    #move=[]
     for m in move:
         keyboard.press(m)
         sleep(0.1)
@@ -138,18 +139,20 @@ while p.poll() is None: #Start loop
         except:
             pass
 
-        sleep(5)
-        try:
-            coins_now=len(list(py.locateAllOnScreen('coin_color.png',confidence=0.8,region=(luc[0], luc[1], rdc[0], rdc[1]))))
-        except:
-            coins_now = 0
+        t=0
+        delta_reward=0
+        live=1
+        sleep(1.5)
+        sc = mss.mss().grab(monitor)
+        pic = ImageOps.grayscale(Image.frombytes("RGB", sc.size, sc.bgra, "raw", "BGRX"))
+        coins_now = max(0,len(list(locateAll(coin,pic,confidence=0.8))))
+        continue
 
-    if locate(ys, pic,grayscale=True) != None:
+    if locate(ys, pic,grayscale=True,confidence=0.98) != None:
         delta_reward -= 1
         if coins_now != 0:
             bon = coins_now
-            #coins_now = len(list(py.locateAllOnScreen('coin_color.png', confidence=0.8,region=(luc[0], luc[1], rdc[0], rdc[1]))))
-            coins_now = len(list(locateAll(coin,pic)))
+            coins_now = len(list(locateAll(coin,pic,confidence=0.8)))
             delta_reward += 10*(bon-coins_now)
     else:
         if pic.getpixel((1,30)) == 220:
@@ -158,35 +161,34 @@ while p.poll() is None: #Start loop
         print(f"attempt {live} fails - score:{delta_reward}")
         # zapisz jako wynik sesji t
         #UZWGLĘDNIJ ZE T-1 FAILOWAŁO //zmodyfikować
-        sleep(0.2)
-        coins_now = len(list(py.locateAllOnScreen('coin_color.png', confidence=0.8
-                                              , region=(luc[0], luc[1], rdc[0], rdc[1]))))
-        print("COIN NOW:",coins_now)
+        f.write(f"{live},{t-1},{move},{delta_reward},-100\n")
+        sleep(1)
+        coins_now = max(0,len(list(locateAll(coin,pic,confidence=0.8))))
+
+        delta_reward = 0
         live += 1
         t=0
+        continue
 
     t += 1
 
     #terminations
-    if t==50: #per lvl-specified in the future
+    if t==101: #per lvl-specified in the future
         delta_reward -= 50
         print(f"attempt {live} fails [run out of time] - score:{delta_reward}")
         # zapisz jako wynik sesji t
         py.keyDown("r")
         py.keyUp("r")
-        try:
-            coins_now = len(list(py.locateAllOnScreen('coin_color.png', confidence=0.8, region=(luc[0], luc[1], rdc[0], rdc[1]))))
-        except:
-            coins_now = 0
+        sleep(0.2)
+        coins_now = max(0,len(list(locateAll(coin,pic,confidence=0.8))))
 
-        f.write(f"{live},{t-1},{move},{delta_reward}\n")
+        f.write(f"{live},{t-1},{move},{delta_reward},-1\n")
         delta_reward = 0
         live += 1
         t = 0
 
     else:
-        f.write(f"{live},{t-1},{move},{delta_reward}\n")
-
+        f.write(f"{live},{t-1},{move},{delta_reward},-1\n")
 
     print("loop_time:",time()- move_time)
 p.kill()
